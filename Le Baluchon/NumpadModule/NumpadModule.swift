40c611ffd7a3674c7ff385ccf34c4411cc179bd6
Emilio Del Castillo <emilio.del-castillo@outlook.com>
2022-05-03 18:30:53 +0200

    Create NumpadModule

---
+++ Le Baluchon/NumpadModule/NumpadModule.swift
@@ -0,0 +1,49 @@
+//
+//  NumpadModule.swift
+//  Le Baluchon
+//
+//  Created by Emilio Del Castillo on 03/05/2022.
+//
+
+import UIKit
+
+protocol NumpadModuleDelegate: AnyObject {
+    func didPressButton(value: Int, _ sender: UIButton)
+}
+
+@IBDesignable
+class NumpadModule: UIView {
+    @IBOutlet private weak var contentView: UIView!
+    @IBOutlet private var buttons: [UIButton]!
+    
+    public weak var delegate: NumpadModuleDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle(for: NumpadModule.self).loadNibNamed("NumpadModule", owner: self)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(contentView)
+        
+        for (index, element) in buttons.enumerated() {
+            element.setTitle(index.string, for: .normal)
+            element.addTarget(self, action: #selector(didPressButton(sender:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func didPressButton(sender: UIButton) {
+        guard let label = sender.titleLabel,
+              let value = Int(label.text!)
+        else { return }
+        delegate?.didPressButton(value: value, sender)
+    }
+}
